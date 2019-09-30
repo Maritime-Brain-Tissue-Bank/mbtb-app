16248ae6c75aed1502574dfa1b2648d3a445d477
NJ261 <nirav9033@gmail.com>
2019-09-30 15:04:01 -0300

    Added schema for dataset including images

--- mbtb_app/db/menus.sql
+++ mbtb_app/db/menus.sql
@@ -1,38 +1,44 @@
-create table users_menu(
-    id int unsigned not null auto_increment,
-    parent int unsigned default null,
+CREATE TABLE users_menu(
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    parent int unsigned DEFAULT NULL,
     label varchar(255) DEFAULT NULL,
     link varchar(255) DEFAULT NULL,
-    order_number int default null,
-    primary key (id),
-    unique key `index2` (parent, label)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    order_number int DEFAULT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY `index2` (parent, label)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
-insert into users_menu(label, link, order_number) values
+INSERT INTO users_menu(label, link, order_number) VALUES
     ('Home', 'home/',1),
     ('Dataset', 'view_dataset/', 2),
     ('Analysis', 'analysis/', 3),
     ('FAQ', 'faq/',4);
 
-create table admin_menu(
-    id int unsigned not null auto_increment,
-    parent int unsigned default null,
+CREATE TABLE admin_menu(
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    parent int unsigned DEFAULT NULL,
     label varchar(255) DEFAULT NULL,
     link varchar(255) DEFAULT NULL,
-    order_number int default null,
-    primary key (id),
-    unique key `index2` (parent, label)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    order_number int DEFAULT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY `index2` (parent, label)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
-insert into admin_menu(label, order_number) values
+INSERT INTO admin_menu(label, order_number) VALUES
     ('Home', 1),
     ('Dataset', 2),
     ('Registration Requests', 3),
     ('FAQ', 4);
 
-insert into admin_menu(label, link, parent, order_number) values
-    ('View Data', 'view_dataset/', (select id from admin_menu where label='Dataset'), 1),
-    ('Add New Data', 'add_new_data/', (select id from admin_menu where  label='Dataset'), 2);
 
-insert into admin_menu(label, link, parent, order_number) value
-    ('View Requests', 'view_registration_requests/', (select id from admin_menu where label='Registration Requests'), 1);
+INSERT INTO admin_menu(label, link, parent, order_number) VALUES
+    ('View Data', 'view_dataset/', (select id from (select * from admin_menu) as `am` WHERE label='Dataset'), 1),
+    ('Add New Data', 'add_new_data/', (select id from (select * from admin_menu) as `am` WHERE  label='Dataset'), 2);
+
+
+INSERT INTO admin_menu(label, link, parent, order_number) VALUE
+    ('View Requests', 'view_registration_requests/', (select id from (select * from admin_menu) as `am` WHERE label='Registration Requests'), 1);
+
+
+UPDATE admin_menu SET link = 'home/' WHERE label = 'Home';
+UPDATE admin_menu SET link = 'faq/' WHERE label = 'FAQ';
--- mbtb_app/db/schema.sql
+++ mbtb_app/db/schema.sql
@@ -1,23 +1,136 @@
-create table `users`(
-    id int unsigned not null auto_increment,
-    email varchar(255) NOT NULL default '',
-    password_hash varchar(255) default NULL,
-    title varchar(10) default null,
-    first_name varchar(255) default null,
-    middle_name varchar(255) default null,
-    last_name varchar(255) default null,
-    institution varchar(255) default NULL,
-    department_name varchar(255) default NULL,
-    position_title varchar(255) default NULL,
-    address_line_1 varchar(255) default NULL,
-    address_line_2 varchar(255) default NULL,
-    city varchar(255) default NULL,
-    province varchar(255) default NULL,
-    country varchar(255) default NULL,
-    postal_code varchar(255) default NULL,
-    comments varchar(255) default NULL,
-    active_since date default null,
-    pending_approval enum('Y','N') default 'Y',
-    primary key (id),
-    unique key email (email)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE users(
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL DEFAULT '',
+    password_hash varchar(255) DEFAULT NULL,
+    title varchar(10) DEFAULT NULL,
+    first_name varchar(255) DEFAULT NULL,
+    middle_name varchar(255) DEFAULT NULL,
+    last_name varchar(255) DEFAULT NULL,
+    institution varchar(255) DEFAULT NULL,
+    department_name varchar(255) DEFAULT NULL,
+    position_title varchar(255) DEFAULT NULL,
+    address_line_1 varchar(255) DEFAULT NULL,
+    address_line_2 varchar(255) DEFAULT NULL,
+    city varchar(255) DEFAULT NULL,
+    province varchar(255) DEFAULT NULL,
+    country varchar(255) DEFAULT NULL,
+    postal_code varchar(255) DEFAULT NULL,
+    comments varchar(255) DEFAULT NULL,
+    active_since date DEFAULT NULL,
+    pending_approval enum('Y','N') DEFAULT 'Y',
+    PRIMARY KEY (id),
+    UNIQUE KEY email (email)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE neurodegenerative_diseases(
+    neuro_diseases_id int unsigned NOT NULL AUTO_INCREMENT,
+    disease_name varchar(255) NOT NULL,
+    PRIMARY KEY (neuro_diseases_id)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+INSERT INTO neurodegenerative_diseases(disease_name) VALUES
+    ('ALCOHOLIC CEREBELLAR DEGENERATION'),
+    ('AMYOTROPHIC LATERAL SCLEROSIS'),
+    ('ALZHEIMER''S DISEASE'),
+    ('BRAIN TUMOUR'),
+    ('CEREBELLAR CORTICAL DEGENERATION'),
+    ('CHRONIC MENIGOENCEPHALITIS'),
+    ('CORTICOBASALGANGLIONIC DEGENERATOIN'),
+    ('DOWN''S SYNDROME'),
+    ('FAHR''S DISEASE'),
+    ('FAMILIAL ALZHEIMER''S DISEASE'),
+    ('FRONTOTEMPORAL DEMENTIA'); -- To Do: Need to add all diseases
+
+
+CREATE TABLE autopsy_type(
+    autopsy_type_id int unsigned NOT NULL AUTO_INCREMENT,
+    autopsy_type varchar(255) NOT NULL,
+    PRIMARY KEY (autopsy_type_id)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+INSERT INTO autopsy_type(autopsy_type) VALUES
+    ('Brain'),
+    ('Brain & Spinal'),
+    ('Full Body');
+
+
+CREATE TABLE tissue_type(
+    tissue_type_id int unsigned NOT NULL AUTO_INCREMENT,
+    tissue_type varchar(255) NOT NULL,
+    PRIMARY KEY (tissue_type_id)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+INSERT INTO tissue_type(tissue_type) VALUES
+    ('Brain'),
+    ('Spinal Cord'),
+    ('Ocular');
+
+
+CREATE TABLE brain_dataset(
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    sex enum('Male', 'Female') DEFAULT NULL,
+    age varchar(50) DEFAULT NULL,
+    postmortem_interval varchar(255) DEFAULT NULL,
+    time_in_fix varchar(255) DEFAULT NULL,
+    neuro_diseases_id int unsigned NOT NULL,
+    tissue_type_id int unsigned NOT NULL,
+    storage_method enum('Fresh', 'Not Fresh') DEFAULT NULL, -- To Do: Yet to be defined
+    storage_year datetime NOT NULL,
+    archive enum('Yes', 'No') DEFAULT 'No',
+    PRIMARY KEY (id),
+    FOREIGN KEY (neuro_diseases_id)
+        REFERENCES neurodegenerative_diseases(neuro_diseases_id)
+        ON DELETE no action,
+    FOREIGN KEY (tissue_type_id)
+        REFERENCES tissue_type(tissue_type_id)
+        ON DELETE no action
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE image_repository(
+    image_id int unsigned NOT NULL AUTO_INCREMENT,
+    id int unsigned NOT NULL,
+    file_name varchar(255) NOT NULL,
+    description text DEFAULT NULL,
+    file_type varchar(255) NOT NULL,
+    file_size int(20) NOT NULL,
+    category enum('gross', 'raw', 'cleaned'), -- To Do: Yet to be defined
+    notes text DEFAULT NULL,
+    date_taken date DEFAULT NULL,
+    date_inserted timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (image_id),
+    FOREIGN KEY (id)
+        REFERENCES brain_dataset(id)
+        ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE dataset_othr_details(
+    id int unsigned NOT NULL,
+    race varchar(255) DEFAULT NULL,
+    diagnosis_of_dementia varchar(255) DEFAULT NULL,
+    duration_of_dementia int(3) DEFAULT NULL,
+    clinical_history varchar(255) DEFAULT NULL,
+    cause_of_death varchar(255) DEFAULT NULL,
+    brain_weight int(5) DEFAULT NULL,
+    neoropathology_detailed text DEFAULT NULL,
+    neuropathology_gross text DEFAULT NULL,
+    neuropathology_micro text DEFAULT NULL,
+    neouropathology_criteria varchar(255) DEFAULT NULL,
+    cerad varchar(255) DEFAULT NULL,
+    braak_stage varchar(255) DEFAULT NULL,
+    khachaturian varchar(255) DEFAULT NULL,
+    abc varchar(255) DEFAULT NULL,
+    autopsy_type_id int unsigned NOT NULL,
+    tissue_type_formalin_fixed enum('True', 'False') DEFAULT NULL,
+    FOREIGN KEY (id)
+        REFERENCES brain_dataset(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (autopsy_type_id)
+        REFERENCES autopsy_type(autopsy_type_id)
+        ON DELETE no action
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
