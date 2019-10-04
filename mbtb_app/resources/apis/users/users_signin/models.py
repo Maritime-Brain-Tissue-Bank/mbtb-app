51f07e047b86cbe4398052b5e69dea9af70fef18
NJ261 <nirav9033@gmail.com>
2019-10-04 14:35:20 -0300

    Added jwt token based auth for users

--- mbtb_app/resources/apis/users/users_signin/models.py
+++ mbtb_app/resources/apis/users/users_signin/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class UsersAccount(models.Model):
+    email = models.CharField(max_length=50)
+    password_hash = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'users'
+        managed = False
