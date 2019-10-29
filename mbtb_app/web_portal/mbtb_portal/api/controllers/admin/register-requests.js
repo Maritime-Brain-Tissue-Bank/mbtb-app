11dcfe9677ef7d9be569b3b72d5944244cf3adaf
NJ261 <nirav9033@gmail.com>
2019-10-29 00:16:50 -0300

    Added integration betwn: sails admin new request and list_new_users api

---
+++ mbtb_app/web_portal/mbtb_portal/api/controllers/admin/register-requests.js
@@ -0,0 +1,44 @@
+const request = require('request');
+
+module.exports = {
+
+
+  friendlyName: 'Register requests',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+    success: {
+      viewTemplatePath: 'pages/admin_register_requests',
+      locals: {
+        layout: 'layouts/admin_layout'
+      }
+    }
+  },
+
+
+  fn: async function (inputs, exits) {
+    request.get('http://127.0.0.1:8000/list_new_users/', {
+      'headers': {
+        'Authorization': 'Token ' + sails.config.token.token_value,
+      }},
+      function optionalCallback(err, httpResponse, body) {
+        if (err) {
+          console.log({'error_msg': err});
+        }
+        else {
+          const response = JSON.parse(body);
+          return exits.success({data: response});
+        }
+      });
+
+  }
+
+};
--- mbtb_app/web_portal/mbtb_portal/config/routes.js
+++ mbtb_app/web_portal/mbtb_portal/config/routes.js
@@ -51,12 +51,13 @@ module.exports.routes = {
     }
   },
 
-  'GET /view_new_requests': {
-    view: 'pages/admin_register_requests',
+  'GET /view_new_requests':{
+    action: 'admin/register-requests',
     locals: {
       layout: 'layouts/admin_layout'
     }
   },
+
   'GET /add_new_data': {
     view: 'pages/admin_add_new_data',
     locals: {
