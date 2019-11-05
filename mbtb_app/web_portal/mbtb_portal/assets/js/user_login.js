3286d0ac5545f5d0df9e949dd73f464c2c048a22
NJ261 <nirav9033@gmail.com>
2019-11-05 10:13:01 -0400

    Refactored angular controller loading part to avoid inject error and moved jquery and angular to local dependency

--- mbtb_app/web_portal/mbtb_portal/assets/js/user_login.js
+++ mbtb_app/web_portal/mbtb_portal/assets/js/user_login.js
@@ -1,5 +1,9 @@
-var app = angular.module("login_app",[]);
-app.controller('login_controller',["$scope", function($scope)
+var user_login = angular.module('login_app',[]);
+
+user_login.controller('login_controller',['$scope', function($scope)
 {
-  $scope.user={'user_email':'','user_password':''};
+  $scope.user={
+    'user_email': '',
+    'user_password':''
+  };
 }]);
--- mbtb_app/web_portal/mbtb_portal/assets/js/view_single_record.js
+++ mbtb_app/web_portal/mbtb_portal/assets/js/view_single_record.js
@@ -1,8 +1,9 @@
-var app = angular
-  .module("view_single_record_app", [])
-  .controller("view_single_record_controller", ["$scope", "$window", function ($scope, $window) {
+var view_single_record = angular.module("view_single_record_app", []);
+
+view_single_record.controller("view_single_record_controller", ['$scope', '$window', function ($scope, $window) {
 
     $scope.details = $window.mbtb_detailed_data;
 
   }]);
+
 
