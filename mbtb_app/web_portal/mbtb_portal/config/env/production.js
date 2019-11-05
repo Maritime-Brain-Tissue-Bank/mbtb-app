39bbb7df7937234d0294c7b7479f101cd814b8a5
NJ261 <nirav9033@gmail.com>
2019-11-05 14:42:12 -0400

    Removed local dependecy: due to jquery errors, not loading on runtime

--- mbtb_app/web_portal/mbtb_portal/config/env/production.js
+++ mbtb_app/web_portal/mbtb_portal/config/env/production.js
@@ -250,10 +250,10 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
+     onlyAllowOrigins: [
     //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+       'http://localhost:1337',
+     ],
 
 
     /***************************************************************************
