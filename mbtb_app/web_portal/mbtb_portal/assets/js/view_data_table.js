1538676e249b73195650a85e00fdb28bd9e13bce
NJ261 <nirav9033@gmail.com>
2019-11-26 12:55:12 -0400

    Modified search feature in admin_view_data_table: search fields are tied to button, data filetering is moved from template to controller

--- mbtb_app/web_portal/mbtb_portal/assets/js/view_data_table.js
+++ mbtb_app/web_portal/mbtb_portal/assets/js/view_data_table.js
@@ -4,46 +4,6 @@ myApp.controller('view_data_table_controller', ['$scope', '$filter', '$window', 
 
   $scope.gridOptions = {
     data: [],
-    customFilters: {
-      AgeMax:
-        function (items, value, predicate) {
-          return items.filter(function (item) {
-            return value && item[predicate] ? parseInt(item[predicate]) <= parseInt(value) : true;
-          });
-        },
-      AgeMin:
-        function (items, value, predicate) {
-          return items.filter(function (item) {
-            return value && item[predicate] ? parseInt(item[predicate]) >= parseInt(value) : true;
-          });
-        },
-      PMIMax:
-        function (items, value, predicate) {
-          return items.filter(function (item) {
-            return value && item[predicate] ? parseInt(item[predicate]) <= parseInt(value) : true;
-          });
-        },
-      PMIMin:
-        function (items, value, predicate) {
-          return items.filter(function (item) {
-            return value && item[predicate] ? parseInt(item[predicate]) >= parseInt(value) : true;
-          });
-        },
-
-      TiFMax:
-        function (items, value, predicate) {
-          return items.filter(function (item) {
-            return value && item[predicate] ? parseInt(item[predicate]) <= parseInt(value) : true;
-          });
-        },
-      TiFMin:
-        function (items, value, predicate) {
-          return items.filter(function (item) {
-            return value && item[predicate] ? parseInt(item[predicate]) >= parseInt(value) : true;
-          });
-        },
-
-    }
   };
 
   $scope.gridActions1 = {};
@@ -63,6 +23,45 @@ myApp.controller('view_data_table_controller', ['$scope', '$filter', '$window', 
   });
   $scope.neuro_diagnosis = $scope.neuro_diagnosis.filter(unique);
 
+  $scope.search_fields = {}; // save user choices from form
+  $scope.filtered_data = {
+    data : [], // save filtered data
+
+  };
+
+  // Once search button is pressed, filters are called
+  $scope.submit_search_fields = function(){
+
+    if (document.getElementById("test").style.display === "none")
+      document.getElementById("test").style.display="block";
+
+    // filtering:
+    $scope.filtered_data.data = $filter('filter')($scope.gridOptions.data, {
+      sex: $scope.search_fields.sex, age: $scope.search_fields.age, storage_method: $scope.search_fields.storage_method,
+      tissue_type: $scope.search_fields.tissue_type, neuro_diseases_id: $scope.search_fields.neuro_diseases
+    });
+
+    // To Do: refactor below 3 custom filter for range, `range_filter` should be called once only
+
+    // filtering: time_in_fix
+    $scope.filtered_data.data = $filter('range_filter')($scope.filtered_data.data, {
+      field_name: 'time_in_fix', min_value: $scope.search_fields.tif_min,
+      max_value: $scope.search_fields.tif_max
+    });
+
+    // filtering: postmortem_interval
+    $scope.filtered_data.data = $filter('range_filter')($scope.filtered_data.data, {
+      field_name: 'postmortem_interval', min_value: $scope.search_fields.pmi_min,
+      max_value: $scope.search_fields.pmi_max
+    });
+
+    // filtering: age
+    $scope.filtered_data.data = $filter('range_filter')($scope.filtered_data.data, {
+      field_name: 'age', min_value: $scope.search_fields.age_min,
+      max_value: $scope.search_fields.age_max
+    });
+
+  };
 
   // exporting data or filtered data to csv
   $scope.exportToCsv = function (currentData) {
@@ -83,10 +82,18 @@ myApp.controller('view_data_table_controller', ['$scope', '$filter', '$window', 
   }
 }]);
 
-function myFunction() {
-  if (document.getElementById("test").style.display === "none")
-    document.getElementById("test").style.display="block";
-}
+// custom filter to find values between range
+myApp.filter('range_filter', function () {
+  return function (data, options) {
+    // default min and max values are 0 and 1000
+    let min_value = parseInt(options.min_value) || 0 ;
+    let max_value = parseInt(options.max_value) || 1000;
+
+    if (options.field_name !== undefined){
+      return data.filter((item) => (item[options.field_name] >= min_value && item[options.field_name] <= max_value)
+    )}
+  }
+});
 
 
 
